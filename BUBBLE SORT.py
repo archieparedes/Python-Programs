899ad99374b45f406d905715598eb9f76a8cc233
Archie Paredes <32176834+archieparedes@users.noreply.github.com>
2017-11-30 15:52:22 -0600

    Update BUBBLE SORT.py

--- BUBBLE SORT.py
+++ BUBBLE SORT.py
@@ -1,5 +1,24 @@
+"""
+CSC 243 Python for Programmers
+Updated on Nov 30 Thu 15:27:43 2017
+@author: Archie Paredes
+Bubble Sort
+"""
 def BubbleSort(lst):
-    for i in range(len(lst) - 1, 0, -1):
+    for i in range(len(lst) - 1, 0, -1): # Analyze starting from last
         for j in range(i):
             if(lst[j]> lst[j + 1]):
                 lst[j], lst[j + 1] = lst[j + 1], lst[j]
+    return(lst)
+
+def main():
+    items = int(input('How many elements are in your list? '))
+    lst = []
+    for i in range(items):
+        num = int(input("Please enter integer: "))
+        lst.append(num)
+
+    print(BubbleSort(lst))
+    
+if __name__ == '__main__':
+    main()
